89bf68b66ea8b6583cee31ce4aa423e881bb4499
learnwithjs <jitendrasuthar995@gmail.com>
2023-03-01 12:49:32 +0530

    created sturcture of Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Home from "./routes/Home";
+import Project from "./routes/Project";
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/project" element={<Project/>}/>
+      <Route path="/contact" element={<Contact/>}/>  
+      </Routes>    
+    </>
   );
 }
 
