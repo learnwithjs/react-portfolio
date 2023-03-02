01f7a9bfe40036975178cca8fe7c2dd987c05ae3
learnwithjs <jitendrasuthar995@gmail.com>
2023-03-03 00:43:59 +0530

    crated Pricing card component

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '../components/Footer/Footer'
 import HeroImg2 from '../components/HeroImg2/HeroImg2'
 import Navbar from '../components/Navbar/Navbar'
+import PricingCard from '../components/PricingCard/PricingCard'
 
 const Project = () => {
   return (
     <div>
       <Navbar/>
       <HeroImg2 title="Projects" description="These all are my projects"/>
+      <PricingCard/>
       <Footer/>
     </div>
   )
