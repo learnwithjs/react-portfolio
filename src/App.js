243ef0ffb4ec6939cd77bd88e050eea932a8882f
learnwithjs <jitendrasuthar995@gmail.com>
2023-03-11 00:45:35 +0530

    added dynamic imports of different components inside route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import About from "./routes/About";
-import Contact from "./routes/Contact";
-import Home from "./routes/Home";
-import Project from "./routes/Project";
+// import About from "./routes/About";
+// import Contact from "./routes/Contact";
+// import Home from "./routes/Home";
+// import Project from "./routes/Project";
+
+const Home = React.lazy(() => import("./routes/Home"));
+const About = React.lazy(() => import("./routes/About"));
+const Project = React.lazy(() => import("./routes/Project"));
+const Contact = React.lazy(() => import("./routes/Contact"));
+
 
 function App() {
   return (
     <>
+      <Suspense fallback={<h1>Loading .....</h1>}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/project" element={<Project />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      </Suspense>
     </>
   );
 }
